2908bbfe83f5f2dedd9b7bf177900e80085b1c42
ErickNavarroM <83145677+ErickNavarroM@users.noreply.github.com>
2021-05-06 17:38:05 -0700

    Add files via upload

---
+++ UtileríaGeneral/a.cpp
@@ -0,0 +1,33 @@
+
+void EstablecerVentana(int ancho, int alto)
+{
+    _COORD coord;
+    coord.X = ancho;
+    coord.Y = alto;
+
+    _SMALL_RECT Rect;
+    Rect.Top = 0;
+    Rect.Left = 0;
+    Rect.Bottom = alto - 1;
+    Rect.Right = ancho - 1;
+
+    HANDLE Handle = GetStdHandle(STD_OUTPUT_HANDLE);      // Get Handle
+    SetConsoleScreenBufferSize(Handle, coord);            // Set Buffer Size
+    SetConsoleWindowInfo(Handle, TRUE, &Rect);            // Set Window Size
+}
+//****************************************************************************************
+void MoverCursor(short x,short y)
+{
+    HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
+    COORD coord = {x,y};
+    SetConsoleCursorPosition(handle,coord);
+}
+//****************************************************************************************
+void CambiarCursor(EstadoCursor estado, ModoCursor modo)
+{
+    HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+    CONSOLE_CURSOR_INFO ConCurInf = {modo, estado};
+    SetConsoleCursorInfo(hOut, &ConCurInf);
+}
+//****************************************************************************************
+
